d98566700ed0100e717ed59135d70dd3f60218a8
DmitryGolubenkov <xxdimonxx15@yandex.ru>
2021-08-29 16:27:07 +0300

    Added new API route to get 5 day forecast

--- WeazorApi/Controllers/WeatherController.cs
+++ WeazorApi/Controllers/WeatherController.cs
@@ -32,10 +32,11 @@ public class WeatherController : ControllerBase
     [Route("api/GetCurrentWeather")]
     public async Task<string> GetCurrentWeatherAsync(string cityName, string countryName)
     {
-        string forecast;
+        string currentWeatherJson;
         string cityString = cityName + ',' + countryName;
+        string cacheKey = cityString + "-GetCurrentWeather";
         //Check cache
-        if (!_cache.TryGetValue(cityString, out forecast))
+        if (!_cache.TryGetValue(cacheKey, out currentWeatherJson))
         {
             //If no cached data is found (or it is too old), make a call to API.
             StringBuilder apiCallBuilder = new StringBuilder();
@@ -43,10 +44,35 @@ public class WeatherController : ControllerBase
 
 
             using HttpClient client = new HttpClient();
-            forecast = await client.GetStringAsync(apiCallBuilder.ToString());
+            currentWeatherJson = await client.GetStringAsync(apiCallBuilder.ToString());
 
             //Cache new data
-            _cache.Set(cityString, forecast, new DateTimeOffset(DateTime.Now.AddHours(1)));
+            _cache.Set(cacheKey, currentWeatherJson, new DateTimeOffset(DateTime.Now.AddHours(1)));
+        }
+        return currentWeatherJson;
+    }
+
+    /// <summary>
+    /// API route. Returns 5 day forecast. Uses caching.
+    /// </summary>
+    /// <param name="cityName">City name.</param>
+    /// <param name="countryName">Country name.</param>
+    /// <returns>JSON string containing data about current weather.</returns>
+    [HttpGet]
+    [Route("api/Get5DaysForecast")]
+    public async Task<string> Get5DaysForecastAsync(string cityName, string countryName)
+    {
+        string forecast;
+        string cityString = cityName + ',' + countryName;
+        string cacheKey = cityString + "-Get5DaysForecast";
+        if (!_cache.TryGetValue(cacheKey, out forecast))
+        {
+            StringBuilder apiCall = new StringBuilder();
+            apiCall.Append("https://api.openweathermap.org/data/2.5/forecast?q=").Append(cityString).Append("&appid=").Append(ApiKey);
+
+            using HttpClient client = new HttpClient();
+            forecast = await client.GetStringAsync(apiCall.ToString());
+            _cache.Set(cacheKey, forecast, new DateTimeOffset(DateTime.Now.AddHours(1)));
         }
         return forecast;
     }
